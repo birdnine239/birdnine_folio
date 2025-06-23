50a025540bb16f8658f0d95766d63c89da14f116
버드나인 <birdnine@nate.com>
2025-06-23 13:06:21 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(function(){
-  // 메뉴 호버버
+  // 메뉴 호버
   $('menu ul>li').eq(2).hover(
     function(){
       $('header .sub_menu').stop().slideDown();
@@ -32,19 +32,19 @@ $(function(){
   );
 
   // 가변 폰트 사이즈
-  const divs = document.querySelectorAll('header, .mobile_menu, main');
+  const divs = document.querySelectorAll('header, .mobile_menu, main, .title_hover, .p_title');
 
   divs.forEach(div => {
     const text = div.querySelectorAll('p');
     const subMenuText = div.querySelectorAll('.sub_menu p, .mobile_sub_menu p');
-    const HoverText = div.querySelectorAll('.title_hover p');
+    const HoverText = div.querySelectorAll('.label, .colon, .value');
     
     const resizeObserver = new ResizeObserver(entries => {
       for (let entry of entries) {
         const { width } = entry.contentRect;
     
         // 폰트 크기 계산
-        const fontSize = Math.max(15, Math.min(width / 30, 25));
+        const fontSize = Math.max(15, Math.min(width / 10, 25));
     
         text.forEach(p => {
           p.style.fontSize = `${fontSize}px`;
@@ -53,7 +53,7 @@ $(function(){
           p.style.fontSize = `${fontSize - 5}px`;
         });
         HoverText.forEach(p => {
-          p.style.fontSize = `${fontSize - 8}px`;
+          p.style.fontSize = `${fontSize -3}px`;
         });
       }
     });
@@ -92,9 +92,45 @@ $(function(){
       }
     });
   });
+
+  // popup
+  // $(document).ready(function () {
+  //   $('.e-book').on('click', function (e) {
+  //     e.preventDefault();
+  //     const ebookUrl = $(this).data('ebook');
+  //     if (ebookUrl) {
+  //       $('#ebookIframe').attr('src', ebookUrl);
+  //       $('#ebookPopupOverlay').fadeIn();
+  //     }
+  //   });
+
+  //   $('#ebookPopupOverlay').on('click', function (e) {
+  //     if (e.target.id === 'ebookPopupOverlay') {
+  //       $(this).fadeOut();
+  //       $('#ebookIframe').attr('src', '');
+  //     }
+  //   });  
+  // });
+
+  //포트폴리오 swiper
+  var swiper = new Swiper(".mySwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      renderBullet: function (index, className) {
+        return '<span class="' + className + '">' + (index + 1) + "</span>";
+      },
+    },
+  });
 });
 
 // iframe의 높이를 자동 조정하는 함수
 function adjustIframeHeight(iframe){
   iframe.style.height = iframe.contentWindow.document.body.scrollHeight + "px";
 }
+
+// iframe 팝업 닫기 함수 (부모 창에서 정의)
+function closeEbookPopup() {
+  $('#ebookPopupOverlay').fadeOut();
+  $('#ebookIframe').attr('src', '');
+} 
